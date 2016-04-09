c7748863230d20cdbc21d47dd0112639ed108e1b
Carlos <ing.aragon@tasunsoft.mx>
2016-04-09 16:25:45 -0500

    Base del proyecto

---
+++ nonLinearCurve/src/main/java/org/tasunsoft/App.java
@@ -0,0 +1,13 @@
+package org.tasunsoft;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
